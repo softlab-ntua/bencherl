d995fe01c59a3992799194c3eeb74300b6af9cdb
k4t3r1n4 <k4t3r1n4@gmail.com>
2012-05-13 22:34:10 +0300

    P -> W

--- bench/orbit_par/src/orbit_par.erl
+++ bench/orbit_par/src/orbit_par.erl
@@ -3,7 +3,7 @@
 -export([bench_args/0, run/3]).
 
 bench_args() ->
-	[[F,G,N,P] || F <- [par, par_seq], G <- [fun bench:g124/1], N <- [10000], P <- [8]].
+	[[F,G,N,W] || F <- [par, par_seq], G <- [fun bench:g124/1], N <- [10000], W <- [8]].
 
-run([F,G,N,P|_], _, _) ->
-	io:format("~p~n", [apply(bench, F, [G,N,P])]).
+run([F,G,N,W|_], _, _) ->
+	io:format("~p~n", [apply(bench, F, [G,N,W])]).
