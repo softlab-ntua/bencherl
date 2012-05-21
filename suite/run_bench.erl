58a1f45b1711e3023b58ab6f62eeec0ea4a5a24e
k4t3r1n4 <k4t3r1n4@gmail.com>
2012-05-21 09:21:49 +0300

    -t,-d -> -p, -i, -n

--- suite/run_bench.erl
+++ suite/run_bench.erl
@@ -23,10 +23,16 @@ main() ->
 		{_,Nnames} = lists:keyfind(nodes, 1, Settings),
 		{_,N} = lists:keyfind(nnodes, 1, Settings),
 		{_,S} = lists:keyfind(nschedulers, 1, Settings),
+		{_,Iterations} = lists:keyfind(iterations, 1, Settings),
 		{_,OutFile} = lists:keyfind(outfile, 1, Settings),
 		{_,StatFile} = lists:keyfind(statfile, 1, Settings),
 		{_,DataDir} = lists:keyfind(datadir, 1, Settings),
-	
+		{_,What} = lists:keyfind(what, 1, Settings),
+		NS = case What of
+				node -> N;
+				sched -> S
+			 end,
+
 		% Start the nodes.
 		Nodes = lists:map(fun(Nn)->
 			[Name,Host]=string:tokens(atom_to_list(Nn), "@"),
@@ -37,24 +43,28 @@ main() ->
 
 		% Open the statistics file.				
 		{ok, SF} = file:open(StatFile, [append]),
-		io:format(SF, "~w ~w ", [S, N]),
+		io:format(SF, "~w ", [NS]),
 
 		% Run the benchmark for all argument sets.
 		Fun = fun(Bargs) ->
-			T0 = now(),
-			Coordinator = self(),
-			% In a new process, please.
-			spawn(node(), fun() -> 
-				{ok, OF} = file:open(OutFile, [append]),
-				group_leader(OF, self()),
-				apply(M, run, [Bargs, Nodes, [{datadir, DataDir}]]), 
-				Coordinator ! done,
-				file:close(OF)
-				end
-			),
-			receive done -> ok end,
-			T1 = now(),
-			io:format(SF, "(~w) ~w ", [Bargs, timer:now_diff(T1, T0)/1000]) 		
+
+			Times=lists:map(fun(_) ->
+				T0 = now(),
+				Coordinator = self(),
+				% In a new process, please.
+				spawn(node(), fun() -> 
+					{ok, OF} = file:open(OutFile, [append]),
+					group_leader(OF, self()),
+					apply(M, run, [Bargs, Nodes, [{datadir, DataDir}]]), 
+					Coordinator ! done,
+					file:close(OF)
+					end
+				),
+				receive done -> ok end,
+				T1 = now(),
+				timer:now_diff(T1, T0)/1000
+				end, lists:seq(1,Iterations)),
+			io:format(SF, "(~w) ~w ", [Bargs, median(Times)])	
 		end,
 		lists:foreach(Fun, M:bench_args()),
 
@@ -69,4 +79,14 @@ main() ->
 		E:D ->
 	    io:format("Exception ~p while running benchmark:\n~p\n~p\n", 
 		[E, D, erlang:get_stacktrace()])
+	end.
+
+median(L) ->
+	SL = lists:sort(L),
+	Length = length(SL),
+	case Length band 1 of
+		1 -> lists:nth(round((Length + 1) / 2), SL);
+		0 -> 
+			[A,B] = lists:sublist(SL, round(Length / 2), 2),
+			(A + B) / 2
 	end.
