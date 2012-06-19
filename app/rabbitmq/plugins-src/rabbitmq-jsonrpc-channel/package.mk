ef848b0410f01d397525629b235916756d7f387b
k4t3r1n4 <k4t3r1n4@gmail.com>
2012-06-19 12:20:54 +0200

    Removed rabbitmq.

--- app/rabbitmq/plugins-src/rabbitmq-jsonrpc-channel/package.mk
+++
@@ -1,6 +0,0 @@
-RELEASABLE:=true
-DEPS:=rabbitmq-server rabbitmq-erlang-client erlang-rfc4627-wrapper rabbitmq-mochiweb mochiweb-wrapper rabbitmq-jsonrpc
-
-define construct_app_commands
-	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
-endef
--- app/rabbitmq/plugins-src/rabbitmq-management-visualiser/priv/www/js/visualiser.js
+++
@@ -1,3 +0,0 @@
-dispatcher_add(function(sammy) {});
-
-$("#tabs").append('<li><a href="visualiser/">Visualiser</a></li>');
