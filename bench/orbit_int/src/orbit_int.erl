93ed82b95e32b75e04d522992d5cc8c18043e0e8
k4t3r1n4 <k4t3r1n4@gmail.com>
2012-10-03 22:59:26 +0300

    Fixed orbit_int benchmark based on Kostis' comments.

--- bench/orbit_int/src/orbit_int.erl
+++ bench/orbit_int/src/orbit_int.erl
@@ -4,22 +4,23 @@
 
 bench_args(Version, Conf) ->
     {_,Cores} = lists:keyfind(number_of_cores, 1, Conf),
-	[F1, F2, F3] = case Version of 
-		short -> [fun bench:g13/1, 11, 2];
-		intermediate -> [fun bench:g124/1, 157, 2];
-		long -> [fun bench:g1245/1, 157, 2]
-	end,
-    [[IWP,G,N,W] || IWP <- [true,false], G <- [F1], N <- [F2 * Cores], W <- [F3 * Cores]].
+    [G0,N0,W0] = bench_args_aux(Version),
+    [[IWP,G,N,W] || IWP <- [true,false], G <- [G0],
+        N <- [N0 * Cores], W <- [W0 * Cores]].
+
+bench_args_aux(Version) ->
+    case Version of
+        short -> [fun bench:g13/1, 11, 2];
+        intermediate -> [fun bench:g124/1, 157, 2];
+        long -> [fun bench:g1245/1, 157, 2]
+    end.
 
 run([true,G,N,W|_], [], _) ->
-	io:format("~p~n", [apply(bench, par, [G,N,W])]);
-
+    io:format("~p~n", [bench:par(G,N,W)]);
 run([false,G,N,W|_], [], _) ->
-    io:format("~p~n", [apply(bench, par_seq, [G,N,W])]);
-
+    io:format("~p~n", [bench:par_seq(G,N,W)]);
 run([true,G,N,W|_], Slaves, _) ->
-    io:format("~p~n", [apply(bench, dist, [G,N,W,Slaves])]);
-
+    io:format("~p~n", [bench:dist(G,N,W,Slaves)]);
 run([false,G,N,W|_], Slaves, _) ->
-    io:format("~p~n", [apply(bench, dist_seq, [G,N,W,Slaves])]).
+    io:format("~p~n", [bench:dist_seq(G,N,W,Slaves)]).
 
