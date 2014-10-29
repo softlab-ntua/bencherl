d5fc77e283b8a9b31516d04a24fb693024b9ff4f
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2014-10-29 11:52:13 +0200

    Change im_bench scenario to something bigger

--- bench/im_bench/src/im_bench.erl
+++ bench/im_bench/src/im_bench.erl
@@ -9,13 +9,13 @@ run(_, _, Conf) ->
   %% Setup a coordinator to know when the benchmark finished. This is done by
   %% counting the number of loggers that have finished.
   register(coordinator, self()),
-  launcher:start(2, 1, 2, 10, [greedy], 1),
+  launcher:start(3, 1, 3, 10, [greedy], 1),
   {_,DataDir} = lists:keyfind(datadir, 1, Conf),
-  logger:launch_latency("Bencherl_test", 1, 2, 500, 10, 1, greedy, DataDir ++ "/"),
-  timer:sleep(10000),
-  toxic_client:launch(500, 10, greedy),
-  timer:sleep(10000),
-  toxic_client:launch_traffic(500, 10, greedy),
+  logger:launch_latency("Bencherl_test", 1, 3, 8000, 10, 1, greedy, DataDir ++ "/"),
+  timer:sleep(60000), %XXX: Just to make sure that all clients have logged in.
+  toxic_client:launch(8000, 10, greedy),
+  timer:sleep(60000),
+  toxic_client:launch_traffic(8000, 10, greedy),
   loop(10).
 
 %% loop/1 is a helper function that "prevents" run/3 from finishing until all
