7a41115a387a0eef793764b6bc2dd5911ad2205b
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2014-11-02 14:10:10 +0200

    IM: Get the domain from the slave nodes
    This fix uses one of the slave nodes to parse the hostname. This works because
    the IM application is not designed to work in a distributed environment thus
    all slave nodes should have the same hostname.

--- bench/im_bench/src/im_bench.erl
+++ bench/im_bench/src/im_bench.erl
@@ -5,17 +5,22 @@
 bench_args(_Version, _Conf) ->
   [[]].
 
-run(_, _, Conf) ->
+run(_, Slaves, Conf) ->
   %% Setup a coordinator to know when the benchmark finished. This is done by
   %% counting the number of loggers that have finished.
   register(coordinator, self()),
-  launcher:start(3, 1, 3, 10, ['virtual.softlab.ece.ntua.gr'], 1),
+  %% Get the data dir in order to store the .csv output files there.
   {_,DataDir} = lists:keyfind(datadir, 1, Conf),
-  logger:launch_latency("Bencherl_test", 1, 3, 8000, 10, 1, 'virtual.softlab.ece.ntua.gr', DataDir ++ "/"),
+  %% Find the domain of the slave nodes (currently the same for everyone).
+  Slave1 = hd(Slaves),
+  [_SlaveName, SlaveHost] = string:tokens(atom_to_list(Slave1), "@"),
+  Domain = list_to_atom(SlaveHost),
+  launcher:start(3, 1, 3, 10, [Domain], 1),
+  logger:launch_latency("Bencherl_test", 1, 3, 8000, 10, 1, Domain, DataDir ++ "/"),
   timer:sleep(60000), %XXX: Just to make sure that all clients have logged in.
-  toxic_client:launch(8000, 10, 'virtual.softlab.ece.ntua.gr'),
+  toxic_client:launch(8000, 10, Domain),
   timer:sleep(60000),
-  toxic_client:launch_traffic(8000, 10, 'virtual.softlab.ece.ntua.gr'),
+  toxic_client:launch_traffic(8000, 10, Domain),
   loop(10).
 
 %% loop/1 is a helper function that "prevents" run/3 from finishing until all
